e3153055226d26c840b758964bfd245cd15f7228
raissa0122 <simeonovaraisa@gmail.com>
2023-01-14 19:41:55 +0200

    Create games migration table and Games CRUD

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-th-list"></i> Users</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('game') }}"><i class="nav-icon la la-th-list"></i> Games</a></li>
