360b32be0c8e4d4f5bd2fb06e92283796f83a66e
raissa0122 <simeonovaraisa@gmail.com>
2023-01-15 01:04:11 +0200

    almost redy with the views

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
         //Get data from DB in here and pass it to the view
 
         return view('index.index', [
-            'title' => 'Laravel Project about us',
+            'title' => '<p>Laravel Project about us</p>',
             'text' => '<p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.
             </p>',
