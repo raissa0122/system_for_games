64e2082d4f028785b456d7ab96c5249bd647f5c7
raissa0122 <simeonovaraisa@gmail.com>
2023-01-15 01:21:46 +0200

    update

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,7 +1,9 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+@section('sidebar')
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-th-list"></i> Users</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('game') }}"><i class="nav-icon la la-th-list"></i> Games</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('genre') }}"><i class="nav-icon la la-th-list"></i> Genres</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('creator') }}"><i class="nav-icon la la-th-list"></i> Creators</a></li>
+@endsection
